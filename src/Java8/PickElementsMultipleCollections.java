35d610b0bce234bc33fbc6794ea90b895f5c5e5f
Sumeet Patil <spatil0296@gmail.com>
2023-06-22 17:02:29 -0400

    p1

---
+++ src/Java8/PickElementsMultipleCollections.java
@@ -0,0 +1,32 @@
+package Java8;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+public class PickElementsMultipleCollections {
+    public static void main(String[] args) {
+        final List<String> friends =
+                Arrays.asList("Brian", "Nate", "Neal", "Raju", "Sara", "Scott");
+        final List<String> editors =
+                Arrays.asList("Brian", "Jackie", "John", "Mike");
+        final List<String> comrades =
+                Arrays.asList("Kate", "Ken", "Nick", "Paula", "Zach");
+        final Predicate<String> startsWithN = name -> name.startsWith("N");
+        final long countFriendsStartN =
+                friends.stream()
+                        .filter(startsWithN)
+                        .count();
+        final long countEditorsStartN =
+                editors.stream()
+                        .filter(startsWithN)
+                        .count();
+        final long countComradesStartN =
+                comrades.stream()
+                        .filter(startsWithN)
+                        .count();
+        System.out.println(countFriendsStartN);
+        System.out.println(countEditorsStartN);
+        System.out.println(countComradesStartN);
+
+    }
+}
