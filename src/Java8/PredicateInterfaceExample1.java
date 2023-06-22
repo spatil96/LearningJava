6e571ef606254166c93fdcbf5c8bca289864db1a
Sumeet Patil <spatil0296@gmail.com>
2023-06-22 17:29:37 -0400

    p2

---
+++ src/Java8/PredicateInterfaceExample1.java
@@ -0,0 +1,11 @@
+package Java8;
+
+import java.util.function.Predicate;
+
+public class PredicateInterfaceExample1 {
+    public static void main(String[] args) {
+        Predicate<Integer> isLess = i -> (i<18);
+        System.out.println(isLess.test(6));
+        System.out.print(isLess.test(20));
+    }
+}
