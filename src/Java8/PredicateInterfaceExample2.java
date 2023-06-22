2132bd5fab2dffc5828392c7ab53713cb128f2bb
Sumeet Patil <spatil0296@gmail.com>
2023-06-22 17:54:15 -0400

    predicate examples

---
+++ src/Java8/PredicateInterfaceExample2.java
@@ -0,0 +1,13 @@
+package Java8;
+
+import java.util.function.Predicate;
+
+public class PredicateInterfaceExample2 {
+    public static void main(String[] args) {
+        Predicate<Integer> isLessThan = i -> (i<18);
+        Predicate<Integer> isMoreThan = i -> (i>=10);
+        System.out.println(isMoreThan.and(isLessThan).test(11));
+        System.out.println(isMoreThan.and(isLessThan).negate().test(11));
+        System.out.print(isMoreThan.or(isLessThan).test(5));
+    }
+}
---
+++ src/Java8/PredicateInterfacePassingInFunction.java
@@ -0,0 +1,13 @@
+package Java8;
+
+import java.util.function.Predicate;
+
+public class PredicateInterfacePassingInFunction {
+    public static void main(String[] args) {
+        predicate(10, (i)-> i>70);
+    }
+
+    private static void predicate(int i, Predicate<Integer> predicate) {
+        System.out.println(predicate.test(i));
+    }
+}
