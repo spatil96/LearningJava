6451992e6519b389af1e1210bee9cd55602e54bb
Sumeet Patil <spatil0296@gmail.com>
2023-06-23 09:37:42 -0400

    predicate collection List

--- src/Java8/PickElementsListCollectionUsingStream.java
+++ src/Java8/PickElementsListCollectionUsingStream.java
@@ -3,8 +3,10 @@ package Java8;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-class User {
+/*class User {
     String name, role;
 
     User(String a, String b) {
@@ -35,14 +37,19 @@ class User {
                 result.add(user);
         return result;
     }
-}
-class PickElementsListCollections extends User{
+}*/
+class PickElementsListCollectionsUsingStream extends User{
 
     public static void main(String []args) {
         List<User> users = new ArrayList<>();
         users.add(new User("John", "admin"));
         users.add(new User("Peter", "member"));
-        List<User> admins = User.process(users, (User u) -> u.getRole().equals("admin"));
+        users.add(new User("Erik", "member"));
+        users.add(new User("Andy", "coordinator"));
+//        List<User> admins = User.process(users, (User u) -> u.getRole().equals("admin"));
+//        System.out.println(admins);
+        List<User> admins = users.stream().filter(
+                user -> user.getRole().equals("member")).collect(Collectors.toList());
         System.out.println(admins);
     }
 
