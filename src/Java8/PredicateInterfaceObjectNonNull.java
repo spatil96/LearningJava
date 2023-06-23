9dbdc2a33d95a93d5f2908e614e3b267e0f7cb92
Sumeet Patil <spatil0296@gmail.com>
2023-06-23 09:13:52 -0400

    predicate collection example

---
+++ src/Java8/PredicateInterfaceObjectNonNull.java
@@ -0,0 +1,14 @@
+package Java8;
+
+import java.util.Objects;
+import java.util.function.Predicate;
+
+public class PredicateInterfaceObjectNonNull {
+    public static void main(String[] args) {
+        Predicate<String> isNonNull = Objects::nonNull;
+        Predicate<String> length = i -> i.length() > 8;
+        Predicate<String> length1 = i -> i.length() < 8;
+        String str = "This is a test String";
+        System.out.println(isNonNull.and(length).or(length1).test(str));
+    }
+}
