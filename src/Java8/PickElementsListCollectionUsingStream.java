6d1303f89e636419ad2b9ac762ef83ecf70b40a0
Sumeet Patil <spatil0296@gmail.com>
2023-06-23 09:39:43 -0400

    Code Refractor

--- src/Java8/PickElementsListCollectionUsingStream.java
+++ src/Java8/PickElementsListCollectionUsingStream.java
@@ -6,38 +6,6 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-/*class User {
-    String name, role;
-
-    User(String a, String b) {
-        name = a;
-        role = b;
-    }
-
-    public User() {
-    }
-
-    String getRole() {
-        return role;
-    }
-
-    String getName() {
-        return name;
-    }
-
-    public String toString() {
-        return "User Name : " + getName() + ", Role :" + getRole();
-    }
-
-    public static List<User> process(List<User> users,
-                                     Predicate<User> predicate) {
-        List<User> result = new ArrayList<>();
-        for (User user : users)
-            if (predicate.test(user))
-                result.add(user);
-        return result;
-    }
-}*/
 class PickElementsListCollectionsUsingStream extends User{
 
     public static void main(String []args) {
