4eec6f8cf81ec0c1e7b7c5d9bb82e8d586b3e144
Sumeet Patil <spatil0296@gmail.com>
2023-06-29 15:36:13 -0400

    learning method change

---
+++ src/DSA/LinkedList/SingleLinkedListP1.java
@@ -0,0 +1,7 @@
+package DSA.LinkedList;
+
+public class SingleLinkedListP1 extends ListNode{
+    public static void main(String[] args) {
+                
+    }
+}
---
+++ src/HeadFirstJ5/Chap1/StringArgNameChange.java
@@ -0,0 +1,7 @@
+package HeadFirstJ5.Chap1;
+
+public class StringArgNameChange {
+    public static void main(String[] sumeet) {
+        System.out.println("Hello from different args name");
+    }
+}
