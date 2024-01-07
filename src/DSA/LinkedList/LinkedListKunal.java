a28b6c243a8d2451f49a2856361f58eaecf76476
spatil96 <spatil0296@gmail.com>
2024-01-07 04:53:38 -0500

    Leet code Arrays 101

---
+++ src/DSA/LinkedList/LinkedListKunal.java
@@ -0,0 +1,28 @@
+package DSA.LinkedList;
+
+public class LinkedListKunal {
+    private Node head;
+    private Node tail;
+    private int size=0;
+
+    public LinkedListKunal(int size) {
+        this.size = 0   ;
+    }
+
+    private class Node{
+        private int value;
+        private Node next;
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/LeetCode/Arrays101/DuplicateZeros.java
@@ -0,0 +1,43 @@
+package LeetCode.Arrays101;
+
+public class DuplicateZeros {
+    public static void main(String[] args) {
+        duplicateZeros( new int[] {1,0,2,3,0,4,5,0});
+        duplicateZeros( new int[] {1,2,3});
+    }
+    public static void duplicateZeros(int[] arr) {
+        for(int print :arr){
+            System.out.print(print+" ");
+        }
+        for(int i=0;i< arr.length-1;i++){
+            if(arr[i] == 0){
+                for(int j=i ;j<arr.length-1;j++){
+                    int temp =arr[j++];
+                    arr[j++] = 0;
+                    arr[j+2]=temp;
+
+                }
+            }
+        }
+        for(int print :arr){
+            System.out.println(print);
+        }
+    }
+}
+/*
+Given a fixed-length integer array arr, duplicate each occurrence of zero,
+shifting the remaining elements to the right.
+Note: that elements beyond the length of the original array are not written.
+Do the above modifications to the input array in place and do not return anything.
+Example 1:
+Input: arr = [1,0,2,3,0,4,5,0]
+Output: [1,0,0,2,3,0,0,4]
+Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
+Example 2:
+Input: arr = [1,2,3]
+Output: [1,2,3]
+Explanation: After calling your function, the input array is modified to: [1,2,3]
+Constraints:
+    1 <= arr.length <= 104
+    0 <= arr[i] <= 9
+ */
