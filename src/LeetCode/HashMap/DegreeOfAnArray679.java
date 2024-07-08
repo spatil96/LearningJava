58a9eb3a985e962e0948e85dde65ebf26134e269
Sumeet <spatil0296@gmail.com>
2024-07-08 18:44:54 -0400

    679 LC Easy HashMap

---
+++ src/LeetCode/HashMap/DegreeOfAnArray679.java
@@ -0,0 +1,24 @@
+package LeetCode.HashMap;
+import java.util.*;
+public class DegreeOfAnArray679 {
+    public static void main(String[] args) {
+        System.out.println(findShortestSubArray(new int[]{1,2,2,3,4,1}));
+    }
+    public static int findShortestSubArray(int[] nums) {
+        HashMap<Integer, Integer> nums_counts = new HashMap();
+        HashMap<Integer, Integer> first_seen = new HashMap();
+        int degree = 0;
+        int minLength = 0;
+        for(int i = 0; i<nums.length;i++){
+            first_seen.putIfAbsent(nums[i], i);
+            nums_counts.put(nums[i], nums_counts.getOrDefault(nums[i],0)+1);
+            if(nums_counts.get(nums[i])>degree){
+                degree = nums_counts.get(nums[i]);
+                minLength = i-first_seen.get(nums[i])+1;
+            }else if(nums_counts.get(nums[i])==degree){
+                minLength =Math.min(minLength, i-first_seen.get(nums[i])+1);
+            }
+        }
+        return minLength;
+    }
+}
