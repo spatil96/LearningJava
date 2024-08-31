7df331cc34e5e34272d3aaf6b1a459d42f6da8cd
Sumeet Patil <spatil0296@gmail.com>
2024-08-31 01:03:00 -0400

    GFG Start

---
+++ src/GFG/PrimeNumber.java
@@ -0,0 +1,27 @@
+package GFG;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        long number = 29999990000000000L; // Example number
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+
+    static boolean isPrime(long n) {
+        if (n <= 1) return false;
+        if (n <= 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+
+        // Check divisors from 5 to sqrt(n)
+        for (long i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
